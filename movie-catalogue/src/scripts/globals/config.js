bccae32ed20aba40a8f1b8928eacb4d0cae91e8e
AchmadLyraa <89878825+AchmadLyraa@users.noreply.github.com>
2024-11-03 12:30:09 +0800

    Add files via upload

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+	KEY: "8f199aabcd705e39997e20db9a0d2da1",
+	BASE_URL: "https://api.themoviedb.org/3/",
+	BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+	DEFAULT_LANGUAGE: "en-us",
+	CACHE_NAME: "MovieCatalogue-V1",
+};
+
+export default CONFIG;
+
+// API KEY = 8f199aabcd705e39997e20db9a0d2da1
+
+// API READ ACCESS TOKEN = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZjE5OWFhYmNkNzA1ZTM5OTk3ZTIwZGI5YTBkMmRhMSIsIm5iZiI6MTczMDYwNzg1NC43NDQ4MjYzLCJzdWIiOiI2NzI2MWVkOWZmZjMzYjY2ZWJjYTJhMzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.q7coV-TqKRsHuSlzV2Vvc92PjFDq6Mm7uipTzISevn0
